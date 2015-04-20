3726eed0aad5fd542ea7fb9f33564397d062b691
AntyaDev <antyadev@gmail.com>
2015-04-20 13:52:04 +0300

    added ShoppingCart demo project.

---
+++ 2015/IsCSharpStillEnough/ShoppingCart/Domain.fs
@@ -0,0 +1,45 @@
+﻿module Domain
+
+type ProductId = System.Guid
+type CartItemId = System.Guid
+
+type CartItem = {
+   Id : CartItemId
+   Product : ProductId
+}
+
+type EmptyState = NoItems    
+
+type ActiveState = {
+   UnpaidItems : CartItem list
+}
+
+type PaidForState = { 
+   PaidItems : CartItem list
+   Payment : decimal 
+}
+
+type Cart = 
+   | Empty of EmptyState
+   | Active of ActiveState
+   | PaidFor of PaidForState 
+
+   
+let addToEmptyCart item = 
+   Cart.Active { UnpaidItems = [item] }
+
+let addToActiveCart cart item = 
+   let items = item :: cart.UnpaidItems
+   Cart.Active { cart with UnpaidItems = items }
+
+let removeFromActiveCart cart item = 
+   let items = cart.UnpaidItems 
+               |> List.filter (fun i -> i <> item)                
+                  
+   match items with
+   | [] -> Cart.Empty NoItems
+   | _ -> Cart.Active { cart with UnpaidItems = items }
+
+let payForActiveCart cart amount =   
+   Cart.PaidFor { PaidItems = cart.UnpaidItems
+                  Payment = amount }
