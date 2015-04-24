a602e2e2063b334497a80b03f367fa8acb92496c
unknown <antyadev@gmail.com>
2015-04-24 12:47:14 +0300

    simplified example

--- 2015/IsCSharpStillEnough/ShoppingCart.FP/Domain.fs
+++ 2015/IsCSharpStillEnough/ShoppingCart.FP/Domain.fs
@@ -1,45 +1,51 @@
 ﻿module Domain
 
-type ProductId = System.Guid
-type CartItemId = System.Guid
+type CartItem = string
 
-type CartItem = {
-   Id : CartItemId
-   Product : ProductId
-}
+type Cart =
+    | Empty
+    | Active of items:CartItem list
+    | PaidFor of items:CartItem list * amount:decimal
+ 
+type Cart with
+  static member New = Cart.Empty
+ 
+  member this.Add item =
+    match this with
+    | Cart.Empty        -> Cart.Active [item]
+    | Cart.Active items -> Cart.Active(item :: items)
+    | Cart.PaidFor _    -> printfn "ERROR: can't add items to a paid cart"
+                           this
+ 
+  member this.Remove item =
+    match this with
+    | Cart.Empty        -> printfn "ERROR: can't remove items from an empty cart"
+                           this
+    
+    | Cart.Active items -> let newItems = items |> List.filter (fun i -> i <> item)
+                           match newItems with
+                           | [] -> Cart.Empty
+                           | _ -> Cart.Active newItems
+    
+    | Cart.PaidFor _    -> printfn "ERROR: can't remove items from a paid cart"
+                           this
+ 
+  member this.Pay amount =
+    match this with
+    | Cart.Active items -> Cart.PaidFor (items, amount)    
+    | _ -> printfn "ERROR: can't pay for a cart that's not active"
+           this
 
-type EmptyState = NoItems    
-
-type ActiveState = {
-   UnpaidItems : CartItem list
-}
-
-type PaidForState = { 
-   PaidItems : CartItem list
-   Payment : decimal 
-}
-
-type Cart = 
-   | Empty of EmptyState
-   | Active of ActiveState
-   | PaidFor of PaidForState 
-
-   
-let addToEmptyCart item = 
-   Cart.Active { UnpaidItems = [item] }
-
-let addToActiveCart cart item = 
-   let items = item :: cart.UnpaidItems
-   Cart.Active { cart with UnpaidItems = items }
-
-let removeFromActiveCart cart item = 
-   let items = cart.UnpaidItems 
-               |> List.filter (fun i -> i <> item)                
-                  
-   match items with
-   | [] -> Cart.Empty NoItems
-   | _ -> Cart.Active { cart with UnpaidItems = items }
-
-let payForActiveCart cart amount =   
-   Cart.PaidFor { PaidItems = cart.UnpaidItems
-                  Payment = amount }
+let emptyCart = Cart.New
+ 
+let cartA = (Cart.New).Add "A"
+ 
+let cartAB = cartA.Add "B"
+ 
+let cartB = cartAB.Remove "A"
+ 
+let emptyCart2 = cartB.Remove "B"
+ 
+let emptyCart3 = emptyCart2.Remove "B"
+ 
+let paidCart = cartAB.Pay 100m
