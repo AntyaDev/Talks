5c18b204de6911cfe5d97fba6c5278c525a5ec56
AntyaDev <antyadev@gmail.com>
2015-10-22 01:41:28 +0200

    added FriendsList.fsproj

---
+++ 2015/DevDay_Kyiv/FriendsList/FriendsList/server/App.fs
@@ -0,0 +1,13 @@
+﻿open Suave
+open Suave.Http.Successful 
+open Suave.Web
+open System.IO
+open System
+open Suave.Http
+open Suave.Http.Applicatives
+
+let indexHtml = File.ReadAllText("client/dest/index.html")
+
+let app = path "/" >>= Writers.setMimeType "text/html" >>= OK indexHtml
+
+startWebServer defaultConfig app
