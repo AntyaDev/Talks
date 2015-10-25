ab2b95fa13d96aa9707e361cad885ddeac4c0397
AntyaDev <antyadev@gmail.com>
2015-10-25 21:23:12 +0100

    started model FriendsList/Domain/FriendItem.fs

---
+++ 2015/DevDay_Kyiv/FriendsListServer/FriendsList/Domain/DomainPrimitiveTypes.fs
@@ -0,0 +1,7 @@
+﻿namespace Domain
+
+module DomainPrimitiveTypes = 
+  
+  type Id = Id of System.Guid
+  
+  type String200 = String200 of string
---
+++ 2015/DevDay_Kyiv/FriendsListServer/FriendsList/Domain/FriendItem.fs
@@ -0,0 +1,19 @@
+﻿namespace Domain
+
+module FriendItem =
+  open Domain.DomainPrimitiveTypes
+
+  type State = {
+    Id: Id
+    FullName: String200
+  }
+ 
+  type Command = 
+    | Add of FullName:String200
+    | Delete
+    | Star
+  
+  type Event = 
+    | Added of id:Id * fullName:String200
+    | Deleted of id:Id
+    | Stared of id:Id
